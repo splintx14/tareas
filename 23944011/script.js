9ec6f9a4d8c8d2430245d90e1bf188d4804be508
Ronald <ronaldmore1424@gmail.com>
2019-11-20 02:52:37 -0400

    tarea 3 de Ronald

---
+++ 23944011/script.js
@@ -0,0 +1,16 @@
+
+window.onload = function(){
+document.getElementById("btitulo").innerHTML = perfil.nombre;
+document.getElementById("ccolor").innerHTML = config.color;
+document.getElementById("jcolor").innerHTML = perfil.color;
+document.getElementById("clibro").innerHTML = config.libro;
+document.getElementById("jlibro").innerHTML = perfil.libro;
+document.getElementById("cmusica").innerHTML = config.musica;
+document.getElementById("jmusica").innerHTML = perfil.musica;
+document.getElementById("cjuego").innerHTML = config.video_juego;
+document.getElementById("jjuego").innerHTML = perfil.video_juego;
+document.getElementById("clenguaje").innerHTML = config.lenguajes;
+document.getElementById("jlenguajes").innerHTML = perfil.lenguajes;
+document.getElementById("cmail").innerHTML = config.email;
+document.getElementById("jmail").innerHTML = perfil.email;
+};
